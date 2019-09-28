d9bc0c2adc144c542c7ddfedde8126fef6a72c0c
puneetamishra1 <38757281+puneetamishra1@users.noreply.github.com>
2019-09-28 16:57:39 +0530

    Commit Test Automation V2

--- Autothon/src/test/java/com/nagarro/Autothon/PageObjects/Admin_HomePage.java
+++ Autothon/src/test/java/com/nagarro/Autothon/PageObjects/Admin_HomePage.java
@@ -4,6 +4,7 @@ import com.nagarro.Autothon.GenericFunctions.BaseTestClass;
 import com.nagarro.Autothon.GenericFunctions.Xls_Reader;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 
 public class Admin_HomePage {
 
@@ -15,16 +16,16 @@ public class Admin_HomePage {
 
     BaseTestClass base = new BaseTestClass();
     public static final String adminMenu_Txt = "//span[text()='Admin Menu']";
-    public static final String addMovie_Lnk = "//a[@class='navLink']";
-    public static final String title_TextField = "//button[@class='btn btn-success']";
-    public static final String director_TextField = "//button[@class='btn btn-success']";
-    public static final String description_TextField = "//button[@class='btn btn-success']";
-    public static final String url_TextField = "//button[@class='btn btn-success']";
-    public static final String saveMovie_Btn = "//button[@class='btn btn-success']";
+    public static final String addMovie_Lnk = "(//a[@class='navLink'])[3]";
+    public static final String title_TextField = "//input[@name='title']";
+    public static final String director_TextField = "//input[@name='director']";
+    public static final String description_TextField = "//textarea[@name='description']";
+    public static final String url_TextField = "//input[@name='file']";
+    public static final String saveMovie_Btn = "(//button[@class='btn btn-success'])[1]";
     public static final String categories_DD = "//select[@name='categories']";
     public static final String categories_DDValues = "//select[@name='categories']/option";
-
-
+    public static final String ratingStar_Txt ="//*[@id=\"main\"]/div/div[6]/div/svg[2]";
+    public static final String moviesList_Txt ="//div[@id='listV2']/div";
 
 
     public void addMovie(int row)
@@ -49,7 +50,18 @@ public class Admin_HomePage {
         base.fill_Txt(url_TextField,url);
         String rating = datatable.getCellData("Add Movie Description", "Rating", row);
         base.log("Select Rating");
+        driver.findElement(By.xpath(ratingStar_Txt)).click();
         driver.findElement(By.xpath(saveMovie_Btn)).click();
 
     }
+
+
+   public void userMovieList()
+   {
+       MovieRentalService_LoginPage loginPage = new MovieRentalService_LoginPage(driver);
+       loginPage.login_user();
+       int size= driver.findElements(By.xpath(moviesList_Txt)).size();
+       String moviesCount =size+"";
+       Assert.assertTrue(moviesCount.equalsIgnoreCase("9"));
+   }
 }
--- Autothon/src/test/java/com/nagarro/Autothon/PageObjects/MovieRentalService_LoginPage.java
+++ Autothon/src/test/java/com/nagarro/Autothon/PageObjects/MovieRentalService_LoginPage.java
@@ -16,11 +16,13 @@ public class MovieRentalService_LoginPage {
     }
 
     BaseTestClass base = new BaseTestClass();
-    public static final String profile_Lnk = "(//a[@id='navLink'])[1]";
-    public static final String login_Lnk = "(//a[@id='navLink'])[2]";
+    public static final String profile_Lnk = "(//a[@class='navLink'])[2]";
+    public static final String profileData_Txt =  "//div[@class='profile']/span/div";
+    public static final String login_Lnk = "(//a[@name='Cancel'])";
     public static final String signIn_Btn = "//button[@class='btn btn-success']";
     public static final String username_TextField = "//input[@name='username']";
     public static final String password_TextField = "//input[@name='password']";
+    public static final String hashIcon_Btn = "//button[@class='btn btn-secondary']";
 
 
 
@@ -29,6 +31,9 @@ public class MovieRentalService_LoginPage {
     {
         base.log("Navigate to Movie Rental Service Page");
         driver.get(URLProviderClass.Return_MovieRentalServiceURL());
+        base.log("Click on hash Icon");
+        driver.findElement(By.xpath(hashIcon_Btn)).click();
+        base.waitUntil(login_Lnk,4);
         Xls_Reader datatable = new Xls_Reader(base.getPropertyValue("autothon_TestAutomation"));
         String username = datatable.getCellData("MoieRentalService_Login", "Username", 2);
         String password = datatable.getCellData("MoieRentalService_Login", "Password", 2);
@@ -36,9 +41,9 @@ public class MovieRentalService_LoginPage {
         driver.findElement(By.xpath(login_Lnk)).click();
         base.waitUntil(username_TextField,5);
         base.log("Fill Username");
-        base.fill_Txt(username_TextField,password);
+        base.fill_Txt(username_TextField,username);
         base.log("Fill Password");
-        base.fill_Txt(password_TextField,username);
+        base.fill_Txt(password_TextField,password);
         driver.findElement(By.xpath(signIn_Btn)).click();
         base.goToSleep(5000);
         base.log("Verify Log In");
@@ -51,6 +56,9 @@ public class MovieRentalService_LoginPage {
     {
         base.log("Navigate to Movie Rental Service Page");
         driver.get(URLProviderClass.Return_MovieRentalServiceURL());
+        base.log("Click on hash Icon");
+        driver.findElement(By.xpath(hashIcon_Btn)).click();
+        base.waitUntil(login_Lnk,4);
         Xls_Reader datatable = new Xls_Reader(base.getPropertyValue("autothon_TestAutomation"));
         String username = datatable.getCellData("MoieRentalService_Login", "Username", 3);
         String password = datatable.getCellData("MoieRentalService_Login", "Password", 3);
@@ -62,7 +70,11 @@ public class MovieRentalService_LoginPage {
         base.log("Fill Password");
         base.fill_Txt(password_TextField,password);
         driver.findElement(By.xpath(signIn_Btn)).click();
+        base.waitUntil(profile_Lnk,5);
         base.log("Verify Log In");
+         driver.findElement(By.xpath(profile_Lnk)).click();
+         base.goToSleep(2000);
+         base.checkElementPresentOrNotInList(profileData_Txt,username);
     }
 
 
